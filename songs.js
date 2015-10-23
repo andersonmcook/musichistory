645a2c51fb5a7f60de909b1a05f858f3ca32adce
Anderson Cook <andersonmcook@gmail.com>
2015-10-23 11:00:57 -0500

    started using json

--- songs.js
+++ songs.js
@@ -1,73 +1,17 @@
+// Part One
+
+// Read from local JSON file with jQuery.
+// Loop over results and inject into Music History list page.
+// Add delete button DOM to each row and use jQuery to delete entire row in the DOM.
+// Part Two
+
+// Take your music and split it into two JSON file instead of them all living on one file.
+// Add a button at the bottom of your music list and label it "More >".
+// Load the songs from the first list and inject the DOM into the document as you've already done.
+// When the user clicks that button, load the songs from the second JSON file and append them to the 
+// bottom of the existing music, but before the More button.
+
 $(document).ready(function() {
-/* Create a branch in your musichistory repository named version2.
-Switch to that branch.
-Requirements
-
-Use JavaScript arrays, loops, and innerHTML to show the music you love.
-
-Students must use JavaScript to create a list of songs in the index.html file for their Music History project. 
-Have them download the songs.js file, which contains an array of strings with song information.
-
-Each student must add one song to the beginning and the end of the array.
-Loop over the array and remove any words or characters that obviously don't belong.
-Students must find and replace the > character in each item with a - character.
-Must add each string to the DOM in index.html in the main content area.
-
-{Song name} by {Artist} on the album {Album}
-*/
-/*
-var songs = [];
-
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-console.log("initial songs", songs)
-songs.push("Conduit - by Converge on the album When Forever Comes Crashing");
-songs.unshift("Blooming Amygdala - by Dendritic Arbor on the album Romantic Love");
-
-console.log("with added songs", songs);
-
-var results = document.getElementById("results");
-var output = "";
-
-for (var i = 0; i < songs.length; i++) {
-	songs[i] = songs[i].replace(/>/g, "-");
-	songs[i] = songs[i].replace(/\*g, "");  //remember to fix this
-	songs[i] = songs[i].replace(/@/g, "");
-	songs[i] = songs[i].replace(/!/g, "");
-	songs[i] = songs[i].replace(/\(/g, "");
-	//songs[i] = songs[i].replace(/by/g, "");
-	output = output + "<p>" + songs[i] + "</p>";
-}
-console.log("songs with characters replaced", songs);
-
-results.innerHTML = output;
-*/
-//testing things without breaking the original
-
-
-/*
-NEW INSTRUCTIONS FOR VERSION4
-
-Time to make Music History into a single page application.
-
-In the navigation bar, make sure you have two links labeled "List Music", and "Add Music".
-Add a DOM element that contains some input fields for the user to enter in the name of a song, 
-	the artist for the song, and the album. 
-	You do not need to enclose them in a <form> element because we're not actually submitting this form anywhere.
-Add a <button> element labeled "Add".
-Now, the entire container for the form should not be displayed when the user first visits your page. 
-	The song list with the corresponding form should be visible.
-When the user clicks on "Add Music", the list view should be hidden, and the music form should show.
-When the user clicks on "List Music", the music form should be hidden, and the list view should show.
-Once the user fills out the song form and clicks the add button, you should collect all values from the input fields,
-	add the song to your array of songs, and update the song list in the DOM.
-*/
-
-
-
 
 var songOptions = $("#song-options-div");
 var results = $("#results");
@@ -85,88 +29,53 @@ var profileLink = $("#profile-link");
 
 var formAddButton = $("#form-add-button");
 
-// when view music is clicked, hides song options
-// viewMusicLink.addEventListener("click", function () {
-// 	songOptions.classList.remove("display-none");
-// 	results.classList.remove("display-none");
-// 	addMusic.classList.add("display-none");
-// });
 
 viewMusicLink.click(function () {
-	// songOptions.classList.remove("display-none");
-	// results.classList.remove("display-none");
-	// addMusic.classList.add("display-none");
 	addMusic.hide();
 	results.fadeIn();
 	songOptions.fadeIn();
-
-
 });
 
-// when add music is clicked, hides song results
-// addMusicLink.addEventListener("click", function () {
-// 	addMusic.classList.remove("display-none");
-// 	results.classList.add("display-none");
-// 	songOptions.classList.add("display-none");
-// });
+
 
 addMusicLink.click(function () {
-	// addMusic.classList.remove("display-none");
-	// results.classList.add("display-none");
-	// songOptions.classList.add("display-none");
 	results.hide();
 	songOptions.hide();
 	addMusic.fadeIn();
 });
 
 
-var songs = [];
+// var songs = [];
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-console.log("initial songs", songs)
-songs.push("Conduit - by Converge on the album When Forever Comes Crashing");
-songs.unshift("Blooming Amygdala - by Dendritic Arbor on the album Romantic Love");
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+// console.log("initial songs", songs)
+// songs.push("Conduit - by Converge on the album When Forever Comes Crashing");
+// songs.unshift("Blooming Amygdala - by Dendritic Arbor on the album Romantic Love");
 
-console.log("with added songs", songs);
+// console.log("with added songs", songs);
 
-// var firstSongName = document.getElementById("first-song-name");
 var output = "";
 var songTitle = "";
 var albumTitle = "";
 var band = "";
-for (var i = 0; i < songs.length; i++) {
-	songs[i] = songs[i].replace(/>/g, "-");
-	songs[i] = songs[i].replace(/\*/g, "");
-	songs[i] = songs[i].replace(/@/g, "");
-	songs[i] = songs[i].replace(/!/g, "");
-	songs[i] = songs[i].replace(/\(/g, "");
-	//songs[i] = songs[i].replace(/by/g, "");
-
-	//output = output + "<p>" + songs[i] + "</p>";
-}
-console.log("songs with characters replaced", songs);
+// for (var i = 0; i < songs.length; i++) {
+// 	songs[i] = songs[i].replace(/>/g, "-");
+// 	songs[i] = songs[i].replace(/\*/g, "");
+// 	songs[i] = songs[i].replace(/@/g, "");
+// 	songs[i] = songs[i].replace(/!/g, "");
+// 	songs[i] = songs[i].replace(/\(/g, "");
+// }
+// console.log("songs with characters replaced", songs);
 
 // add in new songs, artists, albums
-
-// var songNameInput = document.getElementById("song-name-input");
-// var artistNameInput = document.getElementById("artist-name-input");
-// var albumNameInput = document.getElementById("album-name-input");
 
 var songNameInput = $("#song-name-input");
 var artistNameInput = $("#artist-name-input");
 var albumNameInput = $("#album-name-input");
-
-
-// var formAdd = function () {
-// 	if (songNameInput.value.length !== 0 && artistNameInput.value.length !== 0 && albumNameInput.value.length !== 0) {
-// 	songs.unshift(songNameInput.value + " - by " + artistNameInput.value + " on the album " + albumNameInput.value);
-// 	console.log("songs", songs);
-// }
-// };
 
 var formAdd = function () {
 	if (songNameInput.val().length !== 0 && artistNameInput.val().length !== 0 && albumNameInput.val().length !== 0) {
@@ -175,65 +84,66 @@ var formAdd = function () {
 }
 };
 
-// watch for add button to be pressed
 
-//formAddButton.addEventListener("click", formAdd);
+// var songTitles = [];
 
-// var yark = document.querySelector("button[name=add]");
-// yark.addEventListener("click", formAdd);
+// for (var i = 0; i < songs.length; i++) {
+// 		songTitle = songs[i].slice(0, songs[i].indexOf("-") - 1);
+// 		console.log("songTitle", songTitle);
+// 		songTitles.push(songTitle);
+// }
 
-//
+// console.log(songTitles);
 
-var songTitles = [];
+// var albumTitles = [];
 
-for (var i = 0; i < songs.length; i++) {
-		songTitle = songs[i].slice(0, songs[i].indexOf("-") - 1);
-		console.log("songTitle", songTitle);
-		songTitles.push(songTitle);
-}
+// for (var i = 0; i < songs.length; i++) {
+// 		albumTitle = songs[i].slice(songs[i].indexOf("album") + 6);
+// 		console.log("albumTitle", albumTitle);
+// 		albumTitles.push(albumTitle);
+// }
 
-console.log(songTitles);
+// console.log("albumTitles", albumTitles);
 
-var albumTitles = [];
+// var bands = [];
 
-for (var i = 0; i < songs.length; i++) {
-		albumTitle = songs[i].slice(songs[i].indexOf("album") + 6);
-		console.log("albumTitle", albumTitle);
-		albumTitles.push(albumTitle);
-}
+// for (var i = 0; i < songs.length; i++) {
+// 		band = songs[i].slice(songs[i].indexOf("by") + 3, songs[i].indexOf("album") - 8);
+// 		console.log("band", band);
+// 		bands.push(band);
+// }
 
-console.log("albumTitles", albumTitles);
+// console.log("bands", bands);
 
-var bands = [];
+// var writeToResults = function () {
+// 	for (var i = 0; i < songs.length; i++) {
+// 		output += "<div class='song-result'><p class='song-name'>" + songTitles[i] + "</p>";
+// 		output += "<span class='results-line results-artist'>" + bands[i] + "</span>";
+// 		output += "<span class='results-line results-divider'>" + "|" + "</span>";
+// 		output += "<span class='results-line results-album'>" + albumTitles[i] + "</span></div>";
+// 	}
+// 	results.html(output);
+// };
 
-for (var i = 0; i < songs.length; i++) {
-		band = songs[i].slice(songs[i].indexOf("by") + 3, songs[i].indexOf("album") - 8);
-		console.log("band", band);
-		bands.push(band);
-}
-
-console.log("bands", bands);
-//var songTitle = songs[0].slice(0, songs[0].indexOf("-") - 1);
-//console.log("songTitle", songTitle);
-//console.log("firstSongName", firstSongName);
-//firstSongName.innerHTML = "<p>" + firstSong + "</p>";
-
-var writeToResults = function () {
-	for (var i = 0; i < songs.length; i++) {
-		output += "<div class='song-result'><p class='song-name'>" + songTitles[i] + "</p>";
-		output += "<span class='results-line results-artist'>" + bands[i] + "</span>";
-		output += "<span class='results-line results-divider'>" + "|" + "</span>";
-		output += "<span class='results-line results-album'>" + albumTitles[i] + "</span></div>";
+var writeToResults = function (songList) {
+	for (var i = 0; i < songList.songs.length; i++) {
+		var currentSong = songList.songs[i];
+		results.append("<div class='song-result'><p class='song-name'>" + currentSong.title + "</p>");
+		results.append("<span class='results-line results-artist'>" + currentSong.artist + "</span>");
+		results.append("<span class='results-line results-divider'>" + "|" + "</span>");
+		results.append("<span class='results-line results-album'>" + currentSong.album + "</span></div>");
 	}
-	// results.innerHTML = output;
-	results.html(output);
+	// results.html(output);
 };
 
-writeToResults();
+// writeToResults();
+$.ajax({
+  url: "data/songs.json"
+}).done(writeToResults);
 
 
 var addToResults = function () {
-	// if (songNameInput.value.length !== 0 && artistNameInput.value.length !== 0 && albumNameInput.value.length !== 0) {
+
 	if (songNameInput.val().length !== 0 && artistNameInput.val().length !== 0 && albumNameInput.val().length !== 0) {
 
 	var addedSong = "";
@@ -241,17 +151,7 @@ var addToResults = function () {
 	addedSong += "<span class='results-line results-artist'>" + artistNameInput.val() + "</span>";
 	addedSong += "<span class='results-line results-divider'>" + "|" + "</span>";
 	addedSong += "<span class='results-line results-album'>" + albumNameInput.val() + "</span></div>";
-	//results.innerHTML = output;
-	// results.insertAdjacentHTML("afterbegin", addedSong);
-	// results += results.html(addedSong);
 	results.prepend(addedSong);
-	// songNameInput.value = "";
-	// artistNameInput.value = "";
-	// albumNameInput.value = "";
-	// addMusic.classList.add("display-none");
-	// songOptions.classList.remove("display-none");
-	// results.classList.remove("display-none");
-	// errorMessage.classList.add("display-none");
 	errorMessage.hide();
 
 	songNameInput.val("");
@@ -263,21 +163,18 @@ var addToResults = function () {
 	results.show();
 	} else {
 		errorMessage.html("Add a song, artist, and album");
-		// errorMessage.classList.remove("display-none");
 		errorMessage.show();
 	}
 };
 
 var addInputFields = $("#add-music").children();
 
-	// formAddButton.addEventListener("click", formAdd);
 	formAddButton.click(formAdd);
 	addInputFields.keydown(function (event) {
 		if (event.which === 13) {
 			formAdd();
 		}
 	});
-	// formAddButton.addEventListener("click", addToResults);
 	formAddButton.click(addToResults);
 	addInputFields.keydown(function (event) {
 		if (event.which === 13) {
@@ -286,7 +183,6 @@ var addInputFields = $("#add-music").children();
 	});
 
 
-//writeToResults();
 
 
 
@@ -294,7 +190,7 @@ var addInputFields = $("#add-music").children();
 
 
 
-});
+}); //end of jQuery
 
 
 
