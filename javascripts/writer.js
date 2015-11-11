2465d5669de719b4e483690d311c0e61503c58e9
Anderson Cook <andersonmcook@gmail.com>
2015-11-11 09:25:28 -0600

    installed lodash and cleaned up code

--- javascripts/writer.js
+++ javascripts/writer.js
@@ -3,26 +3,23 @@ define(["jquery", "dom", "populate-songs"], function($, dom, populateSongs) {
 
 // runs the json file through handlebars template which writes it to the results div
 		handlebarsToDOM: function (songList) {
-						// function handlebarsToDOM(songList) {
-							require(['hbs!../templates/songs'], function (songTemplate) {
-								$("#results").html(songTemplate(songList));
-							});
-						},
+			require(['hbs!../templates/songs'], function (songTemplate) {
+				$("#results").html(songTemplate(songList));
+			});
+		},
 
 // artist dropdown
 		artistDropdown: function (songList) {
-						// function artistDropdown(songList) {
-							require(['hbs!../templates/artist'], function (artistTemplate) {
-								$("#artists").html(artistTemplate(songList));
-							});
-						},
+			require(['hbs!../templates/artist'], function (artistTemplate) {
+				$("#artists").html(artistTemplate(songList));
+			});
+		},
 
 // album dropdown
 		albumDropdown: function (songList) {
-						// function albumDropdown(songList) {
-							require(['hbs!../templates/album'], function (albumTemplate) {
-								$("#albums").html(albumTemplate(songList));
-							});
-						}
+			require(['hbs!../templates/album'], function (albumTemplate) {
+				$("#albums").html(albumTemplate(songList));
+			});
+		}
   };
 });
