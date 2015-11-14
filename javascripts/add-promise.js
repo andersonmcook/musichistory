3dbfd12490aebf7f7ac5d9d85ed124529a446f0f
Anderson Cook <andersonmcook@gmail.com>
2015-11-13 22:00:04 -0600

    added failure modal for add too

--- javascripts/add-promise.js
+++ javascripts/add-promise.js
@@ -11,13 +11,13 @@ define(["q", "jquery", "bootstrap"], function(Q, $) {
         // Now we can resolve the promise and send the data
         deferred.resolve(json_data);
         console.log("books json_data", json_data);
-        $('#myModal').modal('show');
+        $('#add-success').modal('show');
       })
       // XHR failed for some reason
       .fail(function(xhr, status, error) {
         // Since the call failed, we have to reject the promise
         deferred.reject(error);
-        console.log("error", error);
+        $("#add-failure").modal('show');
       });
 
     return deferred.promise;
