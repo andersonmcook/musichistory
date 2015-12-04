52d878489ac85d14bec9d4f621ab88f7b9d3e878
Anderson Cook <andersonmcook@gmail.com>
2015-12-04 10:12:44 -0600

    add song functionality works. adds to firebase. firebase pulls back that info

--- app/controllers/SongFormCtrl.js
+++ app/controllers/SongFormCtrl.js
@@ -1,9 +1,42 @@
-app.controller("SongFormCtrl", ["$q", "$http", "$scope", "simple-songs", function($q, $http, $scope, simpleSongs) {
+app.controller("SongFormCtrl", ["$q", "$http", "$scope", "simple-songs", "$firebaseArray", function($q, $http, $scope, simpleSongs, $firebaseArray) {
 
-	// $scope.newSong = {};
+	var ref = new Firebase("https://blinding-inferno-367.firebaseio.com/songs/");
 
-	$scope.addSong = function () {
-		simpleSongs.addSong($scope.newSong);
-		$scope.newSong = {};
+	$scope.songs = $firebaseArray(ref);
+	$scope.newSong = {};
+
+	$scope.addSong = function() {
+		$scope.songs.$add({
+			artist: $scope.newSong.artist,
+			title: $scope.newSong.title,
+			album: $scope.newSong.album
+		});
 	};
+	// $scope.addSong = function () {
+	// 	simpleSongs.addSong($scope.newSong);
+	// 	$scope.newSong = {};
+	// };
 }]);
+
+
+
+
+// app.controller("SongFormCtrl", ["$scope", "$firebaseArray",
+//   function($scope, $firebaseArray) {
+
+//     var ref = new Firebase("https://nss-demo-instructor.firebaseio.com/songs");
+//     $scope.songs = $firebaseArray(ref);
+//     $scope.newSong = {};
+
+//     $scope.addSong = function() {
+//       $scope.songs.$add({
+//         artist: $scope.newSong.artist,
+//         name: $scope.newSong.name,
+//         album: {
+//           name: $scope.newSong.albumName,
+//           year: $scope.newSong.albumYear
+//         }
+//       });
+//     };
+//   }
+// ]);
