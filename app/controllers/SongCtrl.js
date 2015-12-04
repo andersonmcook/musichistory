50ecac6ec039640b2692d00c424b8a49b2dc8aef
Anderson Cook <andersonmcook@gmail.com>
2015-12-04 10:17:52 -0600

    deleted more button, fixed input fields. they were writing to the wrong key

--- app/controllers/SongCtrl.js
+++ app/controllers/SongCtrl.js
@@ -2,33 +2,9 @@ app.controller("SongCtrl", ["$q", "$http", "$scope", "simple-songs", "$firebaseA
 
 	$scope.searchTest = "";
 	$scope.songs = [];
-	// $scope.moreSongs = [];
-
-	// $scope.moreSongs = function () {
-	// 	simpleSongs.loadMoreSongs($scope.moreSongs);
-	// }
-
-	// simpleSongs.loadSongs().then(
-	//     function (songs) {
-	//       $scope.songs = simpleSongs.getSongs();
-	//     }, function (error) {
-	//       console.log(error);
-	//     }
-	//   );
 
 	var ref = new Firebase("https://blinding-inferno-367.firebaseio.com/songs/");
 	$scope.songs = $firebaseArray(ref);
 	console.log($scope.songs);
-
-	// $scope.moreSongs = function () {
-	// 	simpleSongs.loadMoreSongs().then(
-	// 			function (songs) {
-	// 				$scope.moreSongs = simpleSongs.getSongs();
-	// 				$scope.songs = $scope.songs.concat($scope.moreSongs);
-	// 			}, function (error) {
-	// 				console.log(error);
-	// 			}
-	// 		);
-	// }
   
 }]);
