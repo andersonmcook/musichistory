c81d1cc09fe59b084b9ca01576145a4a723c9bbb
Anderson Cook <andersonmcook@gmail.com>
2015-12-05 15:28:03 -0600

    halfway working filtering

--- app/controllers/SongCtrl.js
+++ app/controllers/SongCtrl.js
@@ -2,6 +2,8 @@ app.controller("SongCtrl", ["$q", "$http", "$scope", "simple-songs", "$firebaseA
 
 	$scope.searchTest = "";
 	$scope.songs = [];
+	$scope.selectedArtist = $scope.songs.artist;
+	console.log("test", $scope.selectedArtist);
 
 	var ref = new Firebase("https://blinding-inferno-367.firebaseio.com/songs/");
 	$scope.songs = $firebaseArray(ref);
